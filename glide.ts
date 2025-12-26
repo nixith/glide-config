7cb00c7daa542fe0d4ed7824cbfdb1a01a400aa5
nixith <rwstauffer72@gmail.com>
2025-12-26 14:43:28 -0500

    first commit

---
+++ glide.ts
@@ -0,0 +1,266 @@
+//glide.unstable.include("./providers.ts")
+// Config docs:
+//
+//   https://glide-browser.app/config
+//
+// API reference:
+//
+//   https://glide-browser.app/api
+//
+// Default config files can be found here:
+//
+//   https://github.com/glide-browser/glide/tree/main/src/glide/browser/base/content/plugins
+//
+// Most default keymappings are defined here:
+//
+//   https://github.com/glide-browser/glide/blob/main/src/glide/browser/base/content/plugins/keymaps.mts
+//
+
+
+// Try typing `glide.` and see what you can do!
+glide.keymaps.set(
+  "command",
+  "<c-j>",
+  "commandline_focus_next",
+);
+
+glide.keymaps.set(
+  "command",
+  "<c-k>",
+  "commandline_focus_back",
+);
+
+// reverse ordering - works better in my mind
+glide.keymaps.set(
+  "normal",
+  "<C-j>",
+  "tab_prev",
+);
+
+glide.keymaps.set(
+  "normal",
+  "<C-k>",
+  "tab_next",
+);
+
+// glide.styles.add(css`
+//   #TabsToolbar {
+//     visibility: collapse !important;
+//   }
+// `);
+
+
+// pickers
+
+
+glide.keymaps.set("normal", "yf", async () => {
+
+  glide.hints.show({
+    selector: ":any-link",
+    async action({ content }) {
+      const link = await content.execute((target) =>
+        target.getAttribute("href") ?? ""
+      );
+      navigator.clipboard.writeText(link)
+    },
+  })
+
+}, { description: "copy lint at hint" })
+
+
+/*
+ * pick audible tabs (all windows)
+ */
+glide.keymaps.set("normal", "<leader>A", async () => {
+
+  const audible_tabs = await browser.tabs.query({ audible: true })
+
+  glide.commandline.show({
+    title: "open",
+    options: audible_tabs.map((tab) => ({
+      label: tab.title,
+      async execute() {
+        console.log(tab)
+        const windowid = tab.windowId
+        if (windowid === undefined) {
+          return
+        }
+        await browser.windows.update(windowid, { focused: true })
+        await browser.tabs.update(tab.id, {
+          active: true,
+        });
+      },
+    })),
+
+
+  });
+
+}, { description: "Search through tabs playing audio" })
+
+
+/**
+ * custom search providers
+ */
+const search_info: Record<string, { url: string, sep: string }> = {
+  'youtube': {
+    url: "https://www.youtube.com/results?search_query=", sep: "+"
+  }
+} as const
+
+if ("youtube" in search_info) { }
+
+
+/*
+* pick tabs via a selection of bookmarks and history
+*/
+
+glide.keymaps.set("normal", "<leader>o", async () => {
+
+  //let combined: Array<Browser.Bookmarks.BookmarkTreeNode | Browser.History.HistoryItem> = []
+  let combined = []
+  const bookmarks = await browser.bookmarks.getRecent(20);
+  bookmarks.forEach(bmark => combined.push({ title: bmark.title, url: bmark.url }))
+  combined.push(...bookmarks)
+
+  const history = await browser.history.search({ text: "", maxResults: 100 })
+  history.forEach(entry => combined.push({ title: entry.title, url: entry.url }))
+
+  // filtering
+  const newtab = (await browser.runtime.getManifest()).chrome_url_overrides?.newtab
+  const startpage = glide.prefs.get("browser.startup.homepage")
+
+
+  let filtered_combined = combined.filter(e => e.url !== startpage && e.url !== newtab)
+
+
+
+
+  glide.commandline.show({
+    title: "open",
+    options: filtered_combined.map((entry) => ({
+      label: entry.title,
+      async execute({ input: input }) {
+
+
+        // if we find a meatch
+        if (entry.title.includes(input)) {
+          const tab = await glide.tabs.get_first({
+            url: entry.url,
+          });
+          if (tab) {
+            const windowid = tab.windowId;
+            if (windowid === undefined) {
+              return
+            }
+            await browser.windows.update(windowid, {
+              focused: true
+            })
+            await browser.tabs.update(tab.id, {
+              active: true,
+            });
+          } else {
+
+            await browser.tabs.update((await glide.tabs.active()).id, {
+              active: true,
+              url: entry.url,
+            });
+          }
+          // if there isn't a match
+        } else {
+          const terms = input.split(" ",)
+          const first = terms[0]
+          // check if it's a special search term
+          if (terms.length > 1 && first !== undefined && first in search_info) {
+            let info = search_info[first];
+            let query = info?.url + terms.slice(1).join(info?.sep)
+            browser.tabs.update((await glide.tabs.active()).id, {
+              url: query
+            })
+
+          }
+
+          // check if it's (likely) a url we just go to
+          let url: URL;
+          try {
+            url = new URL(input)
+          } catch (_) {
+            try {
+              url = new URL("http://" + input) // firefox automatically makes this https
+
+              // avoids single word searches becoming URLs
+              if (url.hostname.split(".").length == 1 && url.hostname !== "localhost") {
+                throw "probably not a hostname";
+              }
+            } catch (_) { // probably not a url, so look it up in a search engine
+              browser.search.search({
+                query: terms.filter(s => s).join("+"),
+                tabId: (await glide.tabs.active()).id
+              })
+              return
+            }
+          }
+          // so it IS a URL! Just go to it
+          browser.tabs.update((await glide.tabs.active()).id, {
+            url: url.toString()
+          })
+        }
+
+      },
+    })),
+  });
+}, { description: "Open the site searcher" });
+
+
+glide.keymaps.set("normal", "p", async () => {
+  const c = navigator.clipboard
+  const url_maybe = await c.readText()
+  let url;
+  try { // check if clipboard content is a url
+    url = new URL(url_maybe);
+  } catch (_) {
+    return false;
+  }
+
+  const tab = await glide.tabs.active()
+
+  browser.tabs.update(tab.id, {
+    url: url.toString()
+  })
+
+}, { description: "paste url from clipboard" });
+
+
+async function copyFlakeGHFormat(lead: string) { // should work for forgejo, codeberg, gitlab (ideally)
+  const url = (await glide.tabs.active()).url?.split("/")
+  if (url !== undefined && url[3] !== undefined && url[4] !== undefined) {
+    // we want split 4 and 5
+    const flake_url = lead.concat(":", url[3], "/", url[4])
+    await navigator.clipboard.writeText(flake_url)
+  }
+}
+
+
+// copy flake urls
+// github (this pattern match could probably be better)
+glide.autocmds.create("UrlEnter", {
+  hostname: "github.com"
+}, async () => {
+  glide.buf.keymaps.set(
+    "normal",
+    "yf",
+    async () => { await copyFlakeGHFormat("github") }
+  );
+});
+
+
+glide.o.hint_size = "14px";
+// TODO: set up kagi as the search engine when
+// glide can edit the default browser effectively
+
+glide.prefs.set("browser.startup.homepage", "https://kagi.com")
+glide.keymaps.set("normal", "<leader>a", async () => {
+  glide.excmds.execute("commandline_show tab");
+});
+
+
+
---
+++ providers.ts
@@ -0,0 +1,262 @@
+
+export async function newtaburl() {
+  // In the nonewtab version, this will return `null` and upset getURL.
+  // Ternary op below prevents the runtime error.
+  const newtab = (await browser.runtime.getManifest()).chrome_url_overrides?.newtab
+
+  return newtab === null || newtab === undefined ? null : browser.runtime.getURL(newtab)
+}
+
+export type Bookmark = { path: string } & Browser.Bookmarks.BookmarkTreeNode
+
+/**
+ * Search bookmarks, deduplicate and sort by most recent.
+ */
+export async function getBookmarks(query: string): Promise<Bookmark[]> {
+  let bookmarks =
+    // config.get("bmarkfoldersearch") == "true"
+    //? await fuseBookmarksSearch(query):
+    await builtInBookmarksSearch(query)
+
+  // Remove duplicate bookmarks
+  const seen = new Map<string, string>()
+  bookmarks = bookmarks.filter(b => {
+    if (b.url === undefined) { return false }
+    if (seen.get(b.title) === b.url) return false
+    else {
+      seen.set(b.title, b.url)
+      return true
+    }
+  })
+
+  return bookmarks
+}
+
+/**
+ * Uses Browser API to search for bookmark by name and URL.
+ */
+async function builtInBookmarksSearch(query: string): Promise<Bookmark[]> {
+  const bookmarks = await browser.bookmarks.search({ query })
+  return bookmarks
+    .filter(isValidBookmark)
+    .map(b => ({ path: "", ...b }))
+    .sort((a, b) => b.dateAdded - a.dateAdded)
+}
+
+// let allBookmarks: Bookmark[]
+// let bookmarksFuse: Fuse<Bookmark>
+
+/**
+ * Caches all bookmarks to be able to search by any property.
+ * Searches by path, name and URL.
+ */
+// async function fuseBookmarksSearch(query: string): Promise<Bookmark[]> {
+//   allBookmarks = allBookmarks || (await collectBookmarks())
+//   bookmarksFuse =
+//     bookmarksFuse ||
+//     new Fuse(allBookmarks, {
+//       keys: ["path", "title", "url"],
+//       findAllMatches: true,
+//       ignoreLocation: true,
+//       ignoreFieldNorm: true,
+//       threshold: config.get("completionfuzziness"),
+//     })
+//   return query ? bookmarksFuse.search(query).map(r => r.item) : allBookmarks
+// }
+
+async function collectBookmarks(): Promise<Bookmark[]> {
+  const bookmarks = await collectBookmarksAndFolders()
+  const bookmarksDictionary = groupBookmarksById(bookmarks)
+  return bookmarks
+    .filter(isValidBookmark)
+    .map(bookmark => ({
+      path: buildBookmarkPath("", bookmark, bookmarksDictionary),
+      ...bookmark,
+    }))
+    .sort((a, b) => b.dateAdded - a.dateAdded)
+}
+
+function flattenChildren(
+  node: Browser.Bookmarks.BookmarkTreeNode,
+): Browser.Bookmarks.BookmarkTreeNode[] {
+  if (!node.children) {
+    return [node]
+  }
+  return [node, ...node.children.flatMap(flattenChildren)]
+}
+
+function buildBookmarkPath(
+  path: string,
+  bookmark: Browser.Bookmarks.BookmarkTreeNode,
+  bookmarksDictionary: { string?: Browser.Bookmarks.BookmarkTreeNode },
+): string {
+  if (!bookmark.parentId) {
+    return path
+  }
+  //TODO: figure out how to make this type conform
+  const parent: Browser.Bookmarks.BookmarkTreeNode = bookmarksDictionary[bookmark.parentId]
+  return buildBookmarkPath(
+    `${parent.title}/${path}`,
+    parent,
+    bookmarksDictionary,
+  )
+}
+
+/**
+ * Bookmark is not a folder and has valid URL.
+ */
+function isValidBookmark(
+  bookmark: Browser.Bookmarks.BookmarkTreeNode,
+): boolean {
+  try {
+    assert(bookmark.url !== undefined)
+    return !!new URL(bookmark.url)
+  } catch (e) {
+    return false
+  }
+}
+
+let bookmarkPaths: string[]
+
+export async function getBookmarkFolders(query: string) {
+  bookmarkPaths = bookmarkPaths || [
+    ...new Set(
+      (await collectBookmarkFolders())
+        .filter(bookmark => bookmark.path && bookmark.path != "/")
+        .map(bookmark => bookmark.path),
+    ),
+  ]
+  const prefix = !query || query === "/" ? "/" : `.*${query}`
+  const regex = new RegExp(`^${prefix}[^/]*/$`)
+  return bookmarkPaths.filter(path => regex.test(path))
+}
+
+async function collectBookmarkFolders(): Promise<Bookmark[]> {
+  const bookmarks = await collectBookmarksAndFolders()
+  const bookmarksDictionary = groupBookmarksById(bookmarks)
+  return bookmarks
+    .filter(bookmark => bookmark.type === "folder")
+    .map(folder => ({
+      path: buildBookmarkPath(
+        `${folder.title}/`,
+        folder,
+        bookmarksDictionary,
+      ),
+      ...folder,
+    }))
+}
+
+async function collectBookmarksAndFolders(): Promise<
+  Browser.Bookmarks.BookmarkTreeNode[]
+> {
+  const root = await browser.bookmarks.getTree()
+  return root.flatMap(flattenChildren)
+}
+
+function groupBookmarksById(bookmarks: Browser.Bookmarks.BookmarkTreeNode[]): {
+  [id: string]: Browser.Bookmarks.BookmarkTreeNode
+} {
+  return bookmarks.reduce((dict, bookmark) => {
+    dict[bookmark.id] = bookmark
+    return dict
+  }, {})
+}
+
+// export async function getSearchUrls(query: string) {
+//   const suconf = config.get("searchurls")
+//
+//   const searchUrls = []
+//   for (const prop in suconf) {
+//     if (
+//       Object.prototype.hasOwnProperty.call(suconf, prop) &&
+//       prop.startsWith(query)
+//     ) {
+//       searchUrls.push({ title: prop, url: suconf[prop] })
+//     }
+//   }
+//   return searchUrls
+// }
+
+function frecency(item: Browser.History.HistoryItem) {
+  // Doesn't actually care about recency yet.
+
+  return item.visitCount !== undefined ? item.visitCount * -1 : 0
+}
+
+export async function getHistory(
+  query: string,
+): Promise<Browser.History.HistoryItem[]> {
+  // Search history, dedupe and sort by frecency
+  let history = await browser.history.search({
+    text: query,
+    //maxResults: config.get("historyresults"),
+    startTime: 0,
+  })
+
+  // Remove entries with duplicate URLs
+  const dedupe = new Map()
+  for (const page of history) {
+    if (page.url !== await newtaburl()) {
+      if (dedupe.has(page.url)) {
+
+        if (page.title?.length !== undefined && dedupe.get(page.url).title.length < page.title?.length) {
+          dedupe.set(page.url, page)
+        }
+      } else {
+        dedupe.set(page.url, page)
+      }
+    }
+  }
+  history = [...dedupe.values()]
+
+  history.sort((a, b) => frecency(a) - frecency(b))
+
+  return history
+}
+
+export async function getTopSites() {
+  return (await browser.topSites.get()).filter(
+    async page => page.url !== await newtaburl(),
+  )
+}
+
+export async function getCombinedHistoryBmarks(
+  query: string,
+): Promise<Array<{ title: string; url: string }>> {
+  const [history, bookmarks] = await Promise.all([
+    getHistory(query),
+    getBookmarks(query),
+    //getSearchUrls(query),
+  ])
+
+  // Join records by URL, using the title from bookmarks by preference.
+  const combinedMap = new Map<string, any>(
+    bookmarks.map(bmark => [
+      bmark.url,
+      { title: `${bmark.path}${bmark.title}`, url: bmark.url, bmark },
+    ]),
+  )
+  history.forEach(page => {
+    if (combinedMap.has(page.url)) combinedMap.get(page.url).history = page
+    else
+      combinedMap.set(page.url, {
+        title: page.title,
+        url: page.url,
+        history: page,
+      })
+  })
+  // searchUrls.forEach(su => {
+  //   combinedMap.set(su.url, {
+  //     title: su.title,
+  //     url: su.url,
+  //     search: true,
+  //   })
+  // })
+
+  const score = x =>
+    (x.history ? frecency(x.history) : 0) -
+    (x.bmark ? config.get("bmarkweight") : 0) -
+    (x.search ? config.get("searchurlweight") : 0)
+
+  return Array.from(combinedMap.values()).sort((a, b) => score(a) - score(b))
+}
